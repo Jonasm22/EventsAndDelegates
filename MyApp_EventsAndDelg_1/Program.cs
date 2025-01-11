f9080e493c263a7b0d3fc922ac1f4b33dfbde10a
Jonathan Mendoza <john.2114@hotmail.com>
2025-01-11 14:02:53 +0100

    New Lectures Added

---
+++ MyApp_EventsAndDelg_1/Program.cs
@@ -0,0 +1,40 @@
+﻿//Section 11
+//Super Quick intro to Generics
+
+namespace MyApp_EventsAndDelg_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] intArr = { 1, 2, 3, 4, 5 };
+        string[] stringArr = {"one", "two", "three" , "four" };
+       // PrintArray(intArr);
+        //PrintArray(stringArr);
+        
+        string[] stringNames = { "Maria ", "Pedro ", "Liss ", "Mario " };
+        
+        PrintSome(stringNames);
+        PrintSome(intArr);
+    }
+    
+    
+    
+    // a generic Method, that accepts a generic datatype
+    public static void PrintArray<T>(T[]array)
+    {
+        foreach (T item in array)
+        {
+            Console.WriteLine(item);
+        }
+    }
+    
+    
+    public static void PrintSome<Tnames>(Tnames[]array)
+    {
+        foreach (Tnames names in array)
+        {
+            Console.Write(names);
+        }
+    }
+}
---
+++ MyApp_EventsAndDelg_2/Program.cs
@@ -0,0 +1,80 @@
+﻿//Section 11
+//Combining Generic with Delegates to make a sorting algorith
+
+namespace MyApp_EventsAndDelg_2;
+
+class Program
+{
+
+    public delegate int CompareValues<T>(T a, T b);
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class PersonSorter
+    {
+        public void Sort(Person[] people, CompareValues<Person> compare)
+        {
+            for (int i = 0; i < people.Length - 1; i++)
+            {
+                for (int j = i + 1; j < people.Length; j++)
+                {
+                    if (compare(people[i], people[j]) > 0)
+                    {
+                        Person temp = people[i];
+                        people[i] = people[j];
+                        people[j] = temp;
+                    }
+                }
+
+            }
+        }
+    }
+
+
+
+    static void Main(string[] args)
+    {
+        Person[] people =
+        {
+            new Person { Age = 18, Name = "Alice" },
+            new Person { Age = 23, Name = "Bob" },
+            new Person { Age = 31, Name = "Car" },
+            new Person { Age = 42, Name = "Denis" }
+            
+        };
+      
+        PersonSorter sorter = new PersonSorter();
+        sorter.Sort(people, CompareByAge);
+        foreach (Person person in people)
+        {
+            Console.WriteLine($"{person.Name} is {person.Age} years old.");
+            
+        }
+        
+        sorter.Sort(people, CompareByName);
+        foreach (Person person in people)
+        {
+            Console.WriteLine($"{person.Name} is {person.Age} years old.");
+            
+            
+        }
+        Console.ReadKey();
+
+    }
+
+    static int CompareByAge(Person a, Person b)
+    {
+        return a.Age.CompareTo(b.Age);
+    }
+
+    static int CompareByName(Person a, Person b)
+    {
+        return a.Name.CompareTo(b.Name);
+    }
+    
+   
+}
