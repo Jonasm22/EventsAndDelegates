1316822cb3c11831cba44ad9ae7333c3cf0f902f
Jonathan Mendoza <john.2114@hotmail.com>
2025-01-24 01:46:33 +0100

    Delegates Lectures Added

---
+++ MyApp_EventsAndDelg_4/Program.cs
@@ -0,0 +1,102 @@
+﻿namespace MyApp_EventsAndDelegate_4;
+
+//Section 11
+//Multicast Delegate Part 2
+
+class Program
+{
+
+    public delegate void Handler(string message);
+
+    public class Logger
+    {
+        public void LogToConsole(string message)
+        {
+            Console.WriteLine("Log Message: " + message);
+
+        }
+
+        public void LogToFile(string message)
+        {
+            Console.WriteLine("Log File: " + message);
+
+        }
+    }
+
+    internal class Programm
+    {
+        static void Main(string[] args)
+        {
+            // Creating a multicast delegate
+            Logger logger = new Logger();
+            Handler logHandler = logger.LogToConsole;
+            logHandler += logger.LogToFile;
+
+            //invoking the multicast delegate
+            logHandler("Log this Info! ");
+
+            foreach (Handler handler in logHandler.GetInvocationList())
+            {
+                try
+                {
+                    handler("Event ocurred withe error handeling");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                   
+                }
+                
+            }
+            
+            
+
+            //Removing a method from the multicast delegate
+
+            if (isMethodInDelegate(logHandler, logger.LogToFile))
+            {
+                logHandler -= logger.LogToFile;
+
+               Console.WriteLine("LogToFile method removed");
+                
+            }
+            else
+            {
+                Console.WriteLine("LogToFile method not found");
+            }
+            
+
+        }
+
+
+
+        static void InvokeSafely(Handler logHandler, string message)
+        {
+            Handler tempLogHandler = logHandler;
+            if (tempLogHandler != null)
+            {
+                tempLogHandler(message);
+            }
+        }
+
+        static bool isMethodInDelegate(Handler handler, Handler methodHandler)
+        {
+            if (handler == null)
+            {
+                return false;
+            }
+
+            foreach (var myDelegate in handler.GetInvocationList())
+            {
+                if (myDelegate == (Delegate)methodHandler)
+                {
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+        
+        
+    }
+}
--- MyApp_EventsAnsDelg_3/Program.cs
+++ MyApp_EventsAnsDelg_3/Program.cs
@@ -1,7 +1,12 @@
-﻿namespace SimpleDelegateExample
-{
+﻿//Section 11
+//Multicast Delegate
 
+namespace MyApp_EventsAnsDelg_3;
+
+class Program
+{
     public delegate void Handler(string message);
+
     public class Logger
     {
         public void LogToConsole(string message)
@@ -17,24 +22,25 @@
         }
     }
 
-    internal class Program
+    internal class Programm
     {
         static void Main(string[] args)
         {
-
+            // Creating a multicast delegate
             Logger logger = new Logger();
             Handler logHandler = logger.LogToConsole;
-            logHandler("Logging To Console");
+            logHandler += logger.LogToFile;
 
-            logHandler=logger.LogToFile;
-            logHandler("Logging To File");
+            //invoking the multicast delegate
+            logHandler("Log this Info! ");
+
+
+            //Removing a method from the multicast delegate
+
+            logHandler -= logger.LogToFile;
+
+            logHandler("After removing logToFile ");
 
         }
-
-
     }
-
-
-
-
 }
